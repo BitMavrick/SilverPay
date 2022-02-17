44f4157fd6de55f8c0070aabe4ec91c01f0c01c9
Mehedi Hasan <mehedi705bd@gmail.com>
2022-02-17 18:02:14 +0600

    Made the foundation

---
+++ login/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'login/logIn.html')
+
+def signup(request):
+    return render(request, 'login/SignUp.html')
+
+def home(request):
+    return render(request, 'login/login.html')
+
+def profile(request):
+    return HttpResponse('This is the profile page')
+
+
